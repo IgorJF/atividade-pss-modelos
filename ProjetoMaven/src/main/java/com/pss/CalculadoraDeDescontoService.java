a731c19d7a4130a58d9fb2f06576534001a0c181
IgorJF <igor.jferraz@yahoo.com>
2026-08-18 01:08:42 -0300

    Classe calculadora de desconto

--- ProjetoMaven/src/main/java/com/pss/CalculadoraDeDescontoService.java
+++ ProjetoMaven/src/main/java/com/pss/CalculadoraDeDescontoService.java
@@ -20,6 +20,33 @@ class CalculadoraDeDescontoService {
     }
     
     public List<CupomDescontoEntrega> calcularDesconto(Pedido pedido){
-        
+        double limite = 10.00;
+
+        for (IFormaDescontoTaxaEntrega metodo : metodosDesconto){
+            if (!metodo.seAplica(pedido)) {
+                continue;//se nao puder aplicar desconto, va para proximo metodo da lista, se puder vira false nao entra no if e segue para o calculo
+            }
+            CupomDescontoEntrega cupom = metodo.calcularDesconto(pedido);
+            
+            double concedido = pedido.getDescontoConcedido();
+            double disponivel = limite - concedido;
+            if (disponivel <= 0) {
+                break;
+            }
+
+            double desconto = cupom.getValorDesconto();
+            if (desconto > disponivel) {
+                desconto = disponivel;
+                cupom = new CupomDescontoEntrega(cupom.getNomeMetodo(), desconto);
+            }
+
+            pedido.getCuponsDescontoEntrega().add(cupom);
+            pedido.aplicarDesconto(desconto);
+
+            if (pedido.getDescontoConcedido() >= limite) {
+                break;
+            }
+        }
+        return pedido.getCuponsDescontoEntrega();
     }
 }
